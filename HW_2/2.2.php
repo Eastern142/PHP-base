a0fdb1601331d629567b174d9aa3e7ad67e26cb2
Eastern142 <Brian_90@mail.ru>
2019-10-06 21:26:39 +0500

    Corrections

--- HW_2/2.2.php
+++ HW_2/2.2.php
@@ -4,22 +4,22 @@ $a = mt_rand(0, 15);
 
 // Вариант №1.
 switch ($a) {
-    case 0: echo "0<br>";
-    case 1: echo "1<br>";
-    case 2: echo "2<br>";
-    case 3: echo "3<br>";
-    case 4: echo "4<br>";
-    case 5: echo "5<br>";
-    case 6: echo "6<br>";
-    case 7: echo "7<br>";
-    case 8: echo "8<br>";
-    case 9: echo "9<br>";
-    case 10: echo "10<br>";
-    case 11: echo "11<br>";
-    case 12: echo "12<br>";
-    case 13: echo "13<br>";
-    case 14: echo "14<br>";
-    case 15: echo 15;
+    case 0: echo $a++ . "<br>";
+    case 1: echo $a++ . "<br>";
+    case 2: echo $a++ . "<br>";
+    case 3: echo $a++ . "<br>";
+    case 4: echo $a++ . "<br>";
+    case 5: echo $a++ . "<br>";
+    case 6: echo $a++ . "<br>";
+    case 7: echo $a++ . "<br>";
+    case 8: echo $a++ . "<br>";
+    case 9: echo $a++ . "<br>";
+    case 10: echo $a++ . "<br>";
+    case 11: echo $a++ . "<br>";
+    case 12: echo $a++ . "<br>";
+    case 13: echo $a++ . "<br>";
+    case 14: echo $a++ . "<br>";
+    case 15: echo $a++ . "<br>";
 }
 
 // Вариант №2.
--- HW_2/2.6.php
+++ HW_2/2.6.php
@@ -1,12 +1,14 @@
 <?php
 
 function power($val, $pow) {
-    if ($pow != 0) {
-        return $val * power($val, $pow - 1);
-    } else {
+    if ($pow == 1) {
         return $val;
+    } elseif ($pow < 0) {
+        return power(1 / $val, -$pow);
     }
+    return $val * power($val, $pow - 1);
 }
 
-echo power(2, 1) . "<br>";
-echo power(2, 3);
+echo power(2, 0) . "<br>";
+echo power(2, -2) . "<br>";
+echo power(2, 2);
