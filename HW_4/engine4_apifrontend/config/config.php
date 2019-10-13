39df7caddb9190b78ee9172e5ec192d96d6a04b5
Eastern142 <Brian_90@mail.ru>
2019-10-14 00:27:05 +0500

    Added engine4_apifrontend.

---
+++ HW_4/engine4_apifrontend/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('TEMPLATES_DIR', '../templates/');
+define('LAYOUTS_DIR', 'layouts/');
+
+require_once "../engine/functions.php";
+require_once "../engine/log.php";
+require_once "../engine/gallery.php";
---
+++ HW_4/engine4_apifrontend/templates/gallery.php
@@ -0,0 +1,6 @@
+<div id="text">
+<?php foreach ($gallery as $filename):?>
+    <a href="/gallery_img/big/<?=$filename?>"><img src="/gallery_img/small/<?=$filename?>"></a>
+
+<?endforeach;?>
+</div>
